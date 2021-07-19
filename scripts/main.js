3eb20ead1451ebda9480eaf6559f581a72486f50
unknown <carlosdanielrod@gmail.com>
2021-07-19 12:35:47 -0500

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+let name = prompt('Dime tu nombre:')
+let age = prompt('Dime tu edad:')
+
+alert(`Soy el bot innecesario y vengo a informarte que tu nombre es ${name} y tienes ${age} años. Que tengas un buen día.`)
